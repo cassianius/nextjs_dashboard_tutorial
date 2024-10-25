e9cba7e2edb8c80b4c94c2d4cb0a66cd4e4cc7ee
David Fischkes <dfischkes@gmail.com>
2024-10-25 18:02:14 -0500

    skinned splash screen

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,24 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from "next/image";
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+
+        <Image
+          src={'/logo_icon.png'}
+          width={44}
+          height={44}
+          // className="hidden md:block"
+          alt="Convocast icon"
+        />
+
+
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <p className="text-[32px] pb-1">convocast</p>
     </div>
   );
 }
